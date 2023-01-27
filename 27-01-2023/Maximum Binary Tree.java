5179d5ca63e1a0a6e60bd7cfb881b7f5c0ec9a44
Pavanshiva Golakoti <85618911+PavanshivaGolakoti@users.noreply.github.com>
2023-01-27 23:26:09 +0530

    Create Maximum Binary Tree.java

---
+++ 27-01-2023/Maximum Binary Tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return construct(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode construct(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int max = left;
+        for (int i = left + 1; i <= right; i++) {
+            if (nums[i] > nums[max]) {
+                max = i;
+            }
+        }
+        TreeNode root = new TreeNode(nums[max]);
+        root.left = construct(nums, left, max - 1);
+        root.right = construct(nums, max + 1, right);
+        return root;
+    }
+}
