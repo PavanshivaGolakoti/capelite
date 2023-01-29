b59f5d4ed64e13bfa3bbf508ab97e3cf28bc9b04
Pavanshiva Golakoti <85618911+PavanshivaGolakoti@users.noreply.github.com>
2023-01-29 22:20:36 +0530

    Update Path Sum 2.java

--- 29-01-2023/Path Sum 2.java
+++ 29-01-2023/Path Sum 2.java
@@ -1,14 +1,16 @@
-List<List<Integer>>res = new ArrayList<>();
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>>res = new ArrayList<>();
     List<Integer>ans = new ArrayList<>();
     
-     helper(root,targetSum,res,ans);
+     help(root,targetSum,res,ans);
     
     return res;
     
 }
 
 
-public void helper(TreeNode root , int targetSum , List<List<Integer>>res , List<Integer>ans){
+public void help(TreeNode root , int targetSum , List<List<Integer>>res , List<Integer>ans){
     
     if(root == null){
         return;
@@ -24,8 +26,8 @@ public void helper(TreeNode root , int targetSum , List<List<Integer>>res , List
     }
     
     ans.add(root.val);
-    helper(root.left , targetSum-root.val , res , ans);
-    helper(root.right,targetSum-root.val , res,ans);
+    help(root.left , targetSum-root.val , res , ans);
+    help(root.right,targetSum-root.val , res,ans);
     ans.remove(ans.size()-1);
     return;
-}
+}}
